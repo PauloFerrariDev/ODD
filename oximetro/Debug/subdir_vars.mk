9b5322873e4a0d57bd3eeb783f6cdf44cea54325
Paulo Ferrari <pf.devcon@gmail.com>
2021-11-09 15:40:56 -0300

    Added oximetro folder

---
+++ oximetro/Debug/subdir_vars.mk
@@ -0,0 +1,47 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5529.cmd 
+
+C_SRCS += \
+../clock.c \
+../i2c.c \
+../main.c \
+../ssd1306.c 
+
+C_DEPS += \
+./clock.d \
+./i2c.d \
+./main.d \
+./ssd1306.d 
+
+OBJS += \
+./clock.obj \
+./i2c.obj \
+./main.obj \
+./ssd1306.obj 
+
+OBJS__QUOTED += \
+"clock.obj" \
+"i2c.obj" \
+"main.obj" \
+"ssd1306.obj" 
+
+C_DEPS__QUOTED += \
+"clock.d" \
+"i2c.d" \
+"main.d" \
+"ssd1306.d" 
+
+C_SRCS__QUOTED += \
+"../clock.c" \
+"../i2c.c" \
+"../main.c" \
+"../ssd1306.c" 
+
+
